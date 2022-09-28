dc9a3e912fa0fb3b1b173121313f15bff6eed58c
Praveena2001 <114354505+Praveena2001@users.noreply.github.com>
2022-09-28 20:48:46 +0530

    Add files via upload

---
+++ Assignment_2 PRAVEENA M/output.py
@@ -0,0 +1,8 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/balas/Desktop/LALITHA IBM/Assignment_2/Assignment_2 LALITHA K/Assignment_2.py
+{'coord': {'lon': 80.2785, 'lat': 13.0878}, 'weather': [{'id': 701, 'main': 'Mist', 'description': 'mist', 'icon': '50n'}], 'base': 'stations', 'main': {'temp': 302.14, 'feels_like': 309.14, 'temp_min': 302.14, 'temp_max': 302.14, 'pressure': 1004, 'humidity': 89}, 'visibility': 4000, 'wind': {'speed': 1.03, 'deg': 0}, 'clouds': {'all': 100}, 'dt': 1664376625, 'sys': {'type': 1, 'id': 9218, 'country': 'IN', 'sunrise': 1664324880, 'sunset': 1664368276}, 'timezone': 19800, 'id': 1264527, 'name': 'Chennai', 'cod': 200}
+temperature is : 302.14
+humidity is : 89
+Temperature is High Alarm On
